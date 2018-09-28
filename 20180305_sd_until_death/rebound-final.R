92d3c46dbbdd16b330ec524710a9cfa47f512d2b
Quentin Geissmann <qgeissmann@gmail.com>
2018-09-28 13:54:49 +0100

    sleep predicts lifespan, for the corrections

--- 20180305_sd_until_death/rebound-final.R
+++ 20180305_sd_until_death/rebound-final.R
@@ -110,9 +110,6 @@ ggplot(stat_rebound_dt, aes(x=overall_sleep_10d, y=days_alive, colour=sex, shape
 				facet_grid( treatment ~ .)
 
 
-
-
-
 ##############################################################################
 mod <- lm( days_alive ~ treatment * sex,dt_stchd[right_censored == F, meta=T])
 summary(mod)
@@ -151,6 +148,25 @@ ggsurv + scale_y_continuous(labels = scales::percent) + scale_x_continuous(name=
 dev.off()
 
 
+boot_km_dt <- surv_data[,{
+					s <-Surv(days_alive, dead)
+					o <- Hmisc::bootkm(s, B=5000)
+					out <- quantile(o, p=c(0.025,.5, 0.975))
+					#print(median(s))
+					as.list(out)
+					}, by= "sex,treatment"]
+					
+
+#~       sex treatment 2.5%  50% 97.5%
+#~ 1:   male        SD 41.5, [38.0, 44.0]
+#~ 2:   male      mock 46.0, [41.0, 48.5]
+#~ 3: female        SD 37.5, [33.0, 38.5]
+#~ 4: female      mock 41.0, [38.5, 44.0]
+
+#41.5, [38.0, 44.0]
+#46.0, [41.0, 48.5]
+#37.5, [33.0, 38.5]
+#41.0, [38.5, 44.0]
 
 #~ ggetho(dt_stchd[xmv(incubator) == 8], aes(y=paste(incubator,sex, id), z=walking)) + 
 #~         stat_tile_etho() + 
@@ -271,17 +287,13 @@ all_pl_objs <- list()
 
 dt_stchd_curated2 <- dt_stchd_curated[, .SD[t < (max(t) - days(1))], by=id] 
 
-all_pl_objs$etho_sleep <- ggetho(dt_stchd_curated2,
+all_pl_objs$etho_sleep_1day <- ggetho(dt_stchd_curated2,
                                     aes(y = asleep, fill=treatment),
                                     summary_time_window=hours(24)) +
 									layers()
-
-all_pl_objs$etho_stimuli <- ggetho(dt_stchd_curated2,
-                                    aes(y = interactions, fill=treatment),
-                                    summary_FUN = sum,
-                                    summary_time_window=hours(24)) +
-									layers() + scale_y_continuous(limits = c(NA,NA))
-
+all_pl_objs$etho_sleep <- ggetho(dt_stchd_curated2,
+                                    aes(y = asleep, fill=treatment)) +
+									layers()
 
 
 
@@ -292,5 +304,66 @@ for(p_name in names(all_pl_objs)){
     print(pl + ggtitle(p_name))
 }
 dev.off()
+
+
+library(boot)
+
+
+foo <- function(data, i){
+	na.omit(data[i])[, (mean(overall_sleep_10d[treatment == "mock"]) - mean(overall_sleep_10d[treatment == "SD"])) / mean(overall_sleep_10d[treatment == "mock"])]
+	}
+
+foo(stat_rebound_dt[sex=="male"], i=1:nrow(stat_rebound_dt))
+b <- boot::boot(data=stat_rebound_dt[sex=="male"], statistic = foo, R =10000)
+boot::boot.ci(b, type="basic")
+#0.9684837
+#0.9452,  1.002
+
+foo(stat_rebound_dt[sex=="female"], i=1:nrow(stat_rebound_dt))
+b <- boot::boot(data=stat_rebound_dt[sex=="female"], statistic = foo, R =10000)
+boot::boot.ci(b, type="basic")
+
+#0.929,[0.893, 0.975],
+
+foo(stat_rebound_dt[], i=1:nrow(stat_rebound_dt))
+b <- boot::boot(data=stat_rebound_dt[], statistic = foo, R =10000)
+boot::boot.ci(b, type="basic")
+
+
+pred_lifespan_dt <- rejoin(tdt[, 
+                        .(mean_sleep = mean(asleep),
+                          mean_sleep_20d = mean(asleep[t < days(20)]),
+                          mean_sleep_10d = mean(asleep[t < days(10)])),
+                          by=id])
+pred_lifespan_dt[, days_alive:= days_alive+2]
+pred_lifespan_dt <- pred_lifespan_dt[treatment=="mock"& right_censored==F]
+
+
+
+
+
+ggplot(pred_lifespan_dt[days_alive > 20 ], aes(mean_sleep_10d, days_alive, colour=sex)) + 
+  geom_point() + 
+  geom_smooth(method="lm")
+
+boom <- pred_lifespan_dt[days_alive > 20 ]
+
+foo <- function(d, i){
+  lm( days_alive ~ mean_sleep_10d * sex, d[i])$coefficients
+  
+}
+
+
+bci <- boot::boot(data=boom, statistic=foo, R=1000)
+boot.ci(bci, type="basic",index=2)
+
+bci <- boot::boot(data=boom[sex=="female"], statistic=foo, R=1000)
+boot.ci(bci, type="basic",index=2)
+
+
+Given the variability of sleep within our wild-type population (fig XXX?), we also wondered whether the sleep amount of untreated animals could predict their lifespan.
+We performed a linear regression (see methods), and found no overall effect: +0.41 days of life per hours of sleep a day  (CI= [-0.51,  1.55]).
+There were also no interaction between time spent sleeping and sex.
+This result indicates that short-sleeper do not die faster.
 
 
